ed6abd2be88577d917d8a3079bf33839f63dd446
Anton Deryabin <ander.2142@gmail.com>
2019-08-21 22:54:04 +0300

    Remove method

--- lunch_bot/lunch_bot.py
+++ lunch_bot/lunch_bot.py
@@ -6,8 +6,7 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import telegram
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    level=logging.INFO)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 logger = logging.getLogger(__name__)
 config = configparser.ConfigParser()
@@ -15,15 +14,12 @@ config.read('../config.ini')
 db = redis.Redis(host='redis')
 
 
-def get_redis():
-    return db
-
-
 def get_places(update):
-    db = get_redis()
-    places = db.smembers(update.message.chat_id)
+    places = db.smembers(update.effective_chat.id)
     return tuple(map(lambda x: x.decode('utf-8'), places))
 
+def is_arg_empty(message_args):
+    return not message_args or not message_args['args'] or len(message_args['args']) == 0
 
 def start(bot, update):
     update.message.reply_text('Не можешь выбрать, куда сходить поесть? Пиши /roll')
@@ -36,6 +32,7 @@ def help(bot, update):
      /roll                     Выбрать место для ланча
      /list                     Вывести весь список мест
      /add <место>              Добавить место
+     /remove <место>           Удалить место
 """)
 
 
@@ -49,15 +46,27 @@ def list_handler(bot, update):
     update.message.reply_text('\n'.join(get_places(update)))
 
 def add(bot, update, **args):
-    db = get_redis()
-    key = update.message.chat_id
-    if not args or not args['args'] or len(args['args']) == 0:
+    key = update.effective_chat.id    
+    if is_arg_empty(args):
         update.message.reply_text('Нужно задать имя местечка, например, так: "/add Кафе у Армена"')
         return
 
     new_place = ' '.join(args['args'])
     db.sadd(key, new_place)
     update.message.reply_text('Местечко "' + new_place + '" добавлено')
+
+
+def remove(bot, update, **args):
+    key = update.effective_chat.id
+    if is_arg_empty(args):
+        update.message.reply_text('Нужно задать имя места, которое нужно удалить')
+        return
+    place = ' '.join(args['args'])
+    if place in get_places(update):
+        db.srem(key, place)
+        update.message.reply_text('Место {0} удалено'.format(place))
+    else:
+        update.message.reply_text('Место {0} отсутствует в списке, удостоверьтесь, что оно там есть с помощью /list'.format(place))
 
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
@@ -73,6 +82,7 @@ def main():
     dp.add_handler(CommandHandler("roll", roll))
     dp.add_handler(CommandHandler("list", list_handler))
     dp.add_handler(CommandHandler("add", add, pass_args=True))
+    dp.add_handler(CommandHandler("remove", remove, pass_args=True)) 
 
     dp.add_error_handler(error)
 
